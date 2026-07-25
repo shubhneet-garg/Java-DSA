64832f4dab3e059690e882bf89d27e82190f4ade
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-07-25 12:18:45 +0530

    Time: 355 ms (62.41%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+CASE 
+WHEN id % 2 = 1 && id != (SELECT MAX(id) FROM Seat)
+THEN ID + 1 
+WHEN ID % 2 = 0
+THEN id - 1
+ELSE ID
+END AS id, student
+FROM Seat
+ORDER BY id
