108b043a20ff488015ebe85a6f70ae44d5a3f557
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-08-04 16:40:59 +0530

    Time: 2 ms (88.56%), Space: 47.9 MB (63.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int duur = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > duur){
+                return false;
+            }
+            duur = Math.max(duur, i + nums[i]);
+        }
+        return true;
+    }
+}
