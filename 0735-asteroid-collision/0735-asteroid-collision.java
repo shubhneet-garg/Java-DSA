29ed7184bad1281c708f3b7d8ca4ab8a0d22eae9
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-08-05 10:13:06 +0530

    Time: 5 ms (68.68%), Space: 47 MB (44.6%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -3,15 +3,15 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         for(int as : asteroids){
             boolean alive = true;
-            
+
             while(alive && !st.isEmpty() && st.peek() > 0 && as < 0){
-                if(st.peek() < -as){
-                    st.pop();
+                if(st.peek() > -as){
+                    alive = false;
                 }else if(st.peek() == -as){
                     st.pop();
                     alive = false;
                 }else{
-                    alive = false;
+                    st.pop();
                 }
             }
             if(alive){
@@ -19,7 +19,7 @@ class Solution {
             }
         }
         int[] ans = new int[st.size()];
-        for(int i = st.size() - 1;i>=0;i--){
+        for(int i=ans.length-1;i>=0;i--){
             ans[i] = st.pop();
         }
         return ans;
