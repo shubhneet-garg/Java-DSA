f7bc36b4eb5252d17790a662b803b4742efe991b
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-08-10 10:28:35 +0530

    Time: 2 ms (88.84%), Space: 47.8 MB (81.77%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,10 +2,9 @@ class Solution {
     public boolean canJump(int[] nums) {
         int duur = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i > duur){
-                return false;
-            }
+            if (i > duur) return false;
             duur = Math.max(duur, i + nums[i]);
+            if (duur >= nums.length - 1) return true;
         }
         return true;
     }
