809ee675a24a6efdceb3a39e780bb6bcc58c395f
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-09-02 13:41:33 +0530

    Time: 1264 ms (88.73%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,3 +1,22 @@
+-- (SELECT u.name AS results
+-- FROM Users u
+-- JOIN MovieRating m
+-- ON u.user_id = m.user_id
+-- GROUP BY u.user_id, u.name
+-- ORDER BY COUNT(*) DESC, u.name ASC
+-- LIMIT 1)
+
+-- UNION ALL
+
+-- (SELECT m.title AS results
+-- FROM Movies m
+-- JOIN MovieRating r
+-- ON m.movie_id = r.movie_id
+-- WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
+-- GROUP BY m.movie_id, m.title
+-- ORDER BY AVG(rating) DESC, title ASC
+-- LIMIT 1)
+
 (SELECT u.name AS results
 FROM Users u
 JOIN MovieRating m
@@ -12,7 +31,7 @@ UNION ALL
 FROM Movies m
 JOIN MovieRating r
 ON m.movie_id = r.movie_id
-WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
+WHERE r.created_at BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY m.movie_id, m.title
 ORDER BY AVG(rating) DESC, title ASC
 LIMIT 1)
