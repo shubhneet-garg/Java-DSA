a92fc0716da571f65751e7b829dcb38d7acbf1a7
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-09-06 18:49:03 +0530

    Time: 15 ms (60.01%), Space: 51.2 MB (93.06%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,14 +2,18 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
+
         int i = 0;
         int j = 0;
+        int count = 0;
+
         while(i < g.length && j < s.length){
             if(s[j] >= g[i]){
+                count++;
                 i++;
             }
             j++;
         }
-        return i;
+        return count;
     }
 }
